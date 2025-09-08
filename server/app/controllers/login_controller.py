3de8c06f22395d0ca26c10e1ed94b701761ce9e6
AgustinHUM <acayrus@correo.um.edu.uy>
2025-09-08 10:29:20 -0300

    datos para testear connectino controller y frontend

--- server/app/controllers/login_controller.py
+++ server/app/controllers/login_controller.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, request
+from flask import jsonify, render_template, redirect, url_for, request
 
 
 def handle_login():
@@ -6,8 +6,11 @@ def handle_login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        #agregar funcion de login
-        #redirect a main si se valida bien
+        #agregar funcion de login y auth
+        #revolver success o fallo a frontend
         #dejo asi para testear supongo
-        return redirect(url_for('pantalla_principal_bp.pantalla_principal'))
+        if username == "testuser" and password == "testpass":
+            return jsonify({"message": "Login successful"}), 200
+        else:
+            return jsonify({"error": "Invalid credentials"}), 401
         #si esta mal volver a login con mensaje de error
--- server/app/controllers/register_controller.py
+++ server/app/controllers/register_controller.py
@@ -1,4 +1,5 @@
-from flask import request, redirect, url_for
+from flask import request, redirect, jsonify
+
 
 def handle_register():
     #cambiar para que matchee con la bsae de datos
@@ -6,8 +7,11 @@ def handle_register():
         email = request.form["email"]
         username = request.form["username"]
         password = request.form["password"]
-        #agregar funcion de registro
-        #redirect a main si son validos los datos
+        #agregar funcion de registro y auth
+        #devolver success o fallo a frontend
         #dejo asi para testear supongo
-        return redirect(url_for('pantalla_principal_bp.pantalla_principal'))
+        if username == "testuser" and password == "testpass" and email == "testemail":
+            return jsonify({"message": "Registration successful"}), 200
+        else:
+            return jsonify({"error": "Invalid credentials"}), 401
         #si esta mal volver a registro con mensaje de error
