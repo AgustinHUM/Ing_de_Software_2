29285347257812e458ec7e20e2126187f119bdb4
LGelpes <lgelpes@correo.um.edu.uy>
2025-09-29 18:58:54 -0300

    db modificada

--- server/app/config.py
+++ server/app/config.py
@@ -10,10 +10,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-    COGNITO_USER_POOL_ID = "us-east-2_LvFG8XSkK"
-    COGNITO_CLIENT_ID = "4fnv0tpv5p871lr5e4nf031q78"
+    COGNITO_USER_POOL_ID = "us-east-2_EDRSvCODn"
+    COGNITO_CLIENT_ID = "9m5l8uln4o6jpm1q7eg3mvl6"
     COGNITO_REGION = "us-east-2"
-    COGNITO_CLIENT_SECRET = "1mprtv9hbl1b71oqmigmje4pks3e3cd8qvi4h7um4nodjq9rrdht"
+    COGNITO_CLIENT_SECRET = "1o0h1u8m8i0le4pkd3u6298a8e1aumkmq4qstuo9mjr54ck48qls"
 
 
     COGNITO_CLIENT = boto3.client("cognito-idp", region_name=COGNITO_REGION)
