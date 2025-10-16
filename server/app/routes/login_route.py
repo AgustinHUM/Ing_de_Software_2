4e31b95daba8a8f0c30a5dd819a0ec53efa9c9cf
LGelpes <lgelpes@correo.um.edu.uy>
2025-10-15 22:12:02 -0300

    Reorganizacion de backend

--- server/app/routes/login_route.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request
-from ..controllers.login_controller import handle_login
-
-login_bp = Blueprint('login', __name__)
-
-@login_bp.route('/login', methods=['POST'])
-def login():
-    return handle_login()
